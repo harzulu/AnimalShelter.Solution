3cf44fe36ab53da257819b494359faac1f9e6bb7
Benjamin McFarland <benrmcfarland@gmail.com>
2021-01-22 10:58:58 -0800

    Fix Controllers for only dog table, add seeded dog data

--- AnimalShelter/Controllers/AnimalShelterController.cs
+++ AnimalShelter/Controllers/AnimalShelterController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using AnimalShelter.Models;
 
 namespace AnimalShelter.Controllers
@@ -29,27 +30,35 @@ namespace AnimalShelter.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<Dog> Get(int id)
         {
-            return "value";
+            return _db.Dogs.FirstOrDefault(entry => entry.DogId == id);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Dog dog)
         {
+            _db.Add(dog);
+            _db.SaveChanges();
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Dog dog)
         {
+            dog.DogId = id;
+            _db.Entry(dog).State = EntityState.Modified;
+            _db.SaveChanges();
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var dogToDelete = _db.Dogs.FirstOrDefault(entry => entry.DogId == id);
+            _db.Dogs.Remove(dogToDelete);
+            _db.SaveChanges();
         }
     }
 }
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -10,6 +10,23 @@ namespace AnimalShelter.Models
     }
 
     public DbSet<Dog> Dogs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Dog>()
+                .HasData(
+                    new Dog { DogId = 1, Name = "Uma", Breed = "Pit Bull"},
+                    new Dog { DogId = 2, Name = "Abner", Breed = "Australian Cattledog"},
+                    new Dog { DogId = 3, Name = "Jackson", Breed = "Lab Mix"},
+                    new Dog { DogId = 4, Name = "Aden", Breed = "Australian Cattledog"},
+                    new Dog { DogId = 5, Name = "Meeks", Breed = "Lab Mix"},
+                    new Dog { DogId = 6, Name = "Brett", Breed = "Retriever"},
+                    new Dog { DogId = 7, Name = "Parker", Breed = "Pit Bull"},
+                    new Dog { DogId = 8, Name = "Miss Precious", Breed = "Chihuahua"},
+                    new Dog { DogId = 9, Name = "Chickie", Breed = "Pit Bull"},
+                    new Dog { DogId = 10, Name = "Jasmine", Breed = "German Sheppard"} 
+                );
+        }
     public DbSet<Cat> Cats { get; set; }
   }
 }
